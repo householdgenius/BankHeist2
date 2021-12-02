0e5c01be226467a2b9e7adf7d7b04ed0d275b045
Emily <emily.reed0913@gmail.com>
2021-12-02 15:38:38 -0600

    3/4 of the way finished

--- Bank.cs
+++ Bank.cs
@@ -5,7 +5,40 @@ public class Bank {
     public int AlarmScore {get; set;}
     public int VaultScore {get; set;}
     public int SecurityGuardScore {get; set;}
-    public bool IsSecure{get;} =false;
+    public bool IsSecure = true;
     
+    public Bank(int cashOnHand, int alarmScore, int vaultScore, int securityGuardScore, bool isSecure)
+    {
+        CashOnHand = cashOnHand;
+        AlarmScore = alarmScore;
+        VaultScore = vaultScore;
+        SecurityGuardScore = securityGuardScore;
+        IsSecure =isSecure;
+    }
+
+    public string MostSecure()
+    {
+        if(SecurityGuardScore < VaultScore && VaultScore > AlarmScore)
+        {
+            return "Vault";
+        }
+        else if(SecurityGuardScore < AlarmScore && AlarmScore > VaultScore)
+        {
+            return "Alarm";
+        }
+        else{ return "Security Guard"; }
+    }
+    public string LeastSecure()
+    {
+        if(SecurityGuardScore > VaultScore && VaultScore < AlarmScore)
+        {
+            return "Vault";
+        }
+        else if(SecurityGuardScore > AlarmScore && AlarmScore < VaultScore)
+        {
+            return "Alarm";
+        }
+        else{ return "Security Guard"; }
+    }
 }
 }
--- Program.cs
+++ Program.cs
@@ -1,57 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 namespace Heist2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Hacker Joe = new Hacker("Joe", 10,20);
-            Hacker John = new Hacker("John", 15,25);
-            Muscle Bob = new Muscle("Bob", 10,20);
-            Muscle Bill = new Muscle("Bill", 15,25);
-            LockSpecialist Sarah = new LockSpecialist("Sarah",10,20);
-            LockSpecialist Sue = new LockSpecialist("Sue",10,20);
+            Hacker Joe = new Hacker("Joe", 10, 20);
+            Hacker John = new Hacker("John", 15, 25);
+            Muscle Bob = new Muscle("Bob", 10, 20);
+            Muscle Bill = new Muscle("Bill", 15, 25);
+            LockSpecialist Sarah = new LockSpecialist("Sarah", 10, 20);
+            LockSpecialist Sue = new LockSpecialist("Sue", 10, 20);
             List<IRobber> rolodex = new List<IRobber>
          {
             Joe, John, Bob, Bill, Sarah, Sue
          };
-         AddCrewMember();
-         
-            void AddCrewMember(){
-            Console.WriteLine($"Current Heist Operatives {rolodex.Count}");
-            Console.WriteLine($"Pick a Crew Member: ");
-            var NewCrewMember = Console.ReadLine();
-           if(NewCrewMember != ""){
-            Console.WriteLine($"{NewCrewMember} can be 1 -Hacker(Disables Alarms), 2 -Muscle(Disarms Guards) 3 -LockSpecialist(cracks vaults) ");
-            var thief = int.Parse(Console.ReadLine());
-            string[] thiefTypes = new string[] { "Null","Hacker", "Muscle", "LockSpecialist" };
-            var thiefType = thiefTypes[thief];
-            Console.WriteLine($"{NewCrewMember} is a {thiefType}");
-            Console.WriteLine($"What will {NewCrewMember}'s skill level be? (1-100)");
-            var skillLevel = Int32.Parse(Console.ReadLine());
-            Console.WriteLine($"{NewCrewMember} has a skill level of {skillLevel}");
-            Console.WriteLine($"What percentage cut does {NewCrewMember} want to recieve?");
-            var percentageCut = Int32.Parse(Console.ReadLine());
-            Console.WriteLine($"{NewCrewMember} will recieve {percentageCut}% of the heist earnings");
-            if(thief == 1){
-           Hacker newHacker = new Hacker(NewCrewMember,skillLevel,percentageCut);
-           rolodex.Add(newHacker);
-            }
-            else if(thief ==2 ){
-            rolodex.Add(new Muscle(NewCrewMember,skillLevel,percentageCut));
-            }
-            else{
-            rolodex.Add(new LockSpecialist(NewCrewMember,skillLevel,percentageCut));
-            }
-            Console.WriteLine("Add another Member?(Y/N): ");
-            var answer = Console.ReadLine().ToLower();
-            if(answer=="y"){
-                AddCrewMember();
-            }
-           }
-           }
+
+
+            Random r = new Random();
+
+            Bank bank = new Bank(r.Next(50_000, 1_000_000), r.Next(0, 100), r.Next(0, 100), r.Next(0, 100), true);
+
+
+            AddCrewMember();
+
+            void AddCrewMember()
+            {
+                Console.WriteLine($"Current Heist Operatives {rolodex.Count}");
+                Console.WriteLine($"Pick a Crew Member: ");
+                var NewCrewMember = Console.ReadLine();
+                if (NewCrewMember != "")
+                {
+                    Console.WriteLine($"{NewCrewMember} can be 1 -Hacker(Disables Alarms), 2 -Muscle(Disarms Guards) 3 -LockSpecialist(cracks vaults) ");
+                    var thief = int.Parse(Console.ReadLine());
+                    string[] thiefTypes = new string[] { "Null", "Hacker", "Muscle", "LockSpecialist" };
+                    var thiefType = thiefTypes[thief];
+                    Console.WriteLine($"{NewCrewMember} is a {thiefType}");
+                    Console.WriteLine($"What will {NewCrewMember}'s skill level be? (1-100)");
+                    var skillLevel = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine($"{NewCrewMember} has a skill level of {skillLevel}");
+                    Console.WriteLine($"What percentage cut does {NewCrewMember} want to recieve?");
+                    var percentageCut = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine($"{NewCrewMember} will recieve {percentageCut}% of the heist earnings");
+                    if (thief == 1)
+                    {
+                        Hacker newHacker = new Hacker(NewCrewMember, skillLevel, percentageCut);
+                        rolodex.Add(newHacker);
+                    }
+                    else if (thief == 2)
+                    {
+                        rolodex.Add(new Muscle(NewCrewMember, skillLevel, percentageCut));
+                    }
+                    else
+                    {
+                        rolodex.Add(new LockSpecialist(NewCrewMember, skillLevel, percentageCut));
+                    }
+                    Console.WriteLine("Add another Member?(Y/N): ");
+                    var answer = Console.ReadLine().ToLower();
+                    if (answer == "y")
+                    {
+                        AddCrewMember();
+                    }
+                    Console.WriteLine($"The most secure system is the {bank.MostSecure()}, and the least secure system is the {bank.LeastSecure()}.");
+                }
+                foreach(IRobber thief in rolodex)
+            {
+            Console.WriteLine();
+            };
             }
         }
     }
+}
 
